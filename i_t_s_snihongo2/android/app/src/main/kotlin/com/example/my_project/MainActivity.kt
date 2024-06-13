f5ed0055b2e0862284ecb9a44557c17d81866384
Hoàng Minh Ngọc <minhngocyn1@gmail.com>
2024-06-13 17:13:18 +0700

    update connect firebase

--- i_t_s_snihongo2/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.itssnihongo2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- noti_app/lib/main.dart
+++ noti_app/lib/main.dart
@@ -1,13 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:noti_app/homepage/homepage.dart';
 
 
-void main() => runApp(MyApp());
+//void main() => runApp(MyApp());
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
+  runApp(MyApp());
+
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }

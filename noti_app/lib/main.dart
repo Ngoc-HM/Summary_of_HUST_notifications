19991be293a29c65cc256b5845b8a7f613a400db
Ngoc-HM <minhngocyn1@gmail.com>
2024-06-17 01:11:18 +0700

    fix

--- noti_app/lib/main.dart
+++ noti_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 import 'firebase_options.dart';
 
 void main() async {
@@ -50,13 +52,50 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  Future<void> writeDataToCSV() async {
+    try {
+      final directory = await getApplicationDocumentsDirectory();
+      final path = '${directory.path}/assets';
+      final filePath = '$path/data.csv';
 
+      // Kiểm tra và tạo thư mục nếu chưa tồn tại
+      final directoryExists = await Directory(path).exists();
+      if (!directoryExists) {
+        await Directory(path).create(recursive: true);
+      }
+
+      final file = File(filePath);
+
+      // Dữ liệu mẫu để ghi vào file CSV
+      String csvData = "title,description,time,datetime\n";
+
+      for (var item in items) {
+        final title = item['title'] ?? 'No title';
+        final description = item['description'] ?? 'No description';
+        final time = item['time'] ?? 'No time';
+        final datetime = item['datetime'] ?? 'No datetime';
+        csvData += '$title,$description,$time,$datetime\n';
+      }
+
+      // Ghi đè dữ liệu vào file CSV
+      await file.writeAsString(csvData);
+      print('File CSV đã được ghi đè thành công.');
+    } catch (e) {
+      print('Có lỗi xảy ra: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Firebase Realtime Database'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: writeDataToCSV,
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: items.length,
@@ -65,6 +104,7 @@ class _HomeScreenState extends State<HomeScreen> {
           return ListTile(
             title: Text(item['title'] ?? 'No title'),
             subtitle: Text(item['description'] ?? 'No description'),
+            trailing: Text(item['datetime'] ?? 'No datetime'),
           );
         },
       ),

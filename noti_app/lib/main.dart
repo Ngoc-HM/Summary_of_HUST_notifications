1b40e754ba1da773fa780c91a32a6ac1df865f32
Hoàng Minh Ngọc <91311915+Ngoc-HM@users.noreply.github.com>
2024-06-17 02:31:53 +0800

    Update main.dart
    delete API

--- noti_app/lib/main.dart
+++ noti_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:noti_app/firebase_api.dart';
 import 'package:noti_app/login/login.dart';
 import 'firebase_options.dart';
 

6fa67a5f6f2d7565e820d82f8a9a220c6639f54b
Hoàng Minh Ngọc <minhngocyn1@gmail.com>
2024-06-17 04:09:59 +0800

    update realtime database

--- noti_app/lib/homepage/homepage.dart
+++ noti_app/lib/homepage/homepage.dart
@@ -1,6 +1,5 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:csv/csv.dart';
-import 'package:flutter/services.dart';
 import 'package:noti_app/bottom_navigator/bottom_navigator.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -9,55 +8,62 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  final databaseRef = FirebaseDatabase.instance.ref().child('data');
+  List<Map<dynamic, dynamic>> items = [];
+  Map<DateTime, List<Map<String, String>>> _events = {};
+
   bool isNotificationOn = true;
   late AnimationController _animationController;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
-  Map<DateTime, List<Map<String, String>>> _events = {};
 
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 300),
-    );
-    _selectedDay = _focusedDay;
-    _loadCSV();
-  }
 
-  Future<void> _loadCSV() async {
-    final data = await rootBundle.loadString('assets/data.csv');
-    final List<List<dynamic>> csvTable = CsvToListConverter().convert(data);
-
-    Map<DateTime, List<Map<String, String>>> events = {};
-    if (data != null)
-      print("CSV load thành công");
-
-    for (var i = 1; i < csvTable.length; i++) {
-      DateTime date = DateTime.parse(csvTable[i][3]);
-      Map<String, String> event = {
-        'title': csvTable[i][0],
-        'description': csvTable[i][1],
-        'time': csvTable[i][2]
-      };
-      if (events[date] == null) {
-        events[date] = [event];
-      } else {
-        events[date]?.add(event);
+    databaseRef.onValue.listen((event) {
+      final data = event.snapshot.value;
+      print(data); // In dữ liệu ra để kiểm tra
+      if (data is Map) {
+        final List<Map<dynamic, dynamic>> loadedItems = [];
+        data.forEach((key, value) {
+          loadedItems.add(Map<dynamic, dynamic>.from(value));
+        });
+        setState(() {
+          items = loadedItems;
+          _convertItemsToEvents();
+        });
+      } else if (data is List) {
+        final List<Map<dynamic, dynamic>> loadedItems = data.map((item) => Map<dynamic, dynamic>.from(item)).toList();
+        setState(() {
+          items = loadedItems;
+          _convertItemsToEvents();
+        });
       }
-    }
-
-    // Sắp xếp các sự kiện theo thời gian
-    events.forEach((key, value) {
-      value.sort((a, b) => a['time']!.compareTo(b['time']!));
     });
 
+    _animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _selectedDay = _focusedDay;
+  }
+
+  void _convertItemsToEvents() {
+    Map<DateTime, List<Map<String, String>>> events = {};
+    for (var item in items) {
+      // Kiểm tra và xử lý giá trị null
+      if (item['datetime'] == null || item['title'] == null || item['description'] == null || item['time'] == null) {
+        continue;
+      }
+      DateTime date = DateTime.parse(item['datetime']);
+      Map<String, String> event = {
+        'title': item['title'] ?? '',
+        'description': item['description'] ?? '',
+        'time': item['time'] ?? ''
+      };
+      events.update(date, (list) => list..add(event), ifAbsent: () => [event]);
+    }
     setState(() {
       _events = events;
-      print("Events loaded: $_events");
     });
   }
 
@@ -70,29 +76,19 @@ class _HomePageState extends State<HomePage>
   void _toggleNotification() {
     setState(() {
       isNotificationOn = !isNotificationOn;
-      if (isNotificationOn) {
-        _animationController.reverse();
-      } else {
-        _animationController.forward();
-      }
+      isNotificationOn ? _animationController.reverse() : _animationController.forward();
     });
   }
 
   List<Map<String, String>> _getEventsForDay(DateTime day) {
-    DateTime normalizedDay = DateTime(day.year, day.month, day.day);
-    print("Getting events for day: $normalizedDay");
-    return _events[normalizedDay] ?? [];
+    return _events[DateTime(day.year, day.month, day.day)] ?? [];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello ITSS in 日本語(2)', style: TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        )),
+        title: Text('Hello ITSS in 日本語(2)', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white)),
         backgroundColor: Colors.orange,
       ),
       body: Column(
@@ -105,42 +101,21 @@ class _HomePageState extends State<HomePage>
                 CircleAvatar(
                   radius: 30,
                   backgroundColor: Colors.grey,
-                  child: ClipOval(
-                    child: Image.asset("assets/images/avatar.png",
-                      fit: BoxFit.cover,
-                      width: 60,
-                      height: 60,
-                    ),
-                  ),
+                  child: ClipOval(child: Image.asset("assets/images/avatar.png", fit: BoxFit.cover, width: 60, height: 60)),
                 ),
                 SizedBox(width: 10),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Hoàng Minh Ngọc',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
+                    Text('Hoàng Minh Ngọc', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
                     Text('20200440', style: TextStyle(fontSize: 14)),
                   ],
                 ),
                 Spacer(),
                 Row(
                   children: [
-                    Icon(
-                      isNotificationOn
-                          ? Icons.notifications_active
-                          : Icons.notifications_off,
-                      color: isNotificationOn ? Colors.orange : Colors.red,
-                    ),
-                    Switch(
-                      value: isNotificationOn,
-                      onChanged: (value) {
-                        setState(() {
-                          isNotificationOn = value;
-                        });
-                      },
-                      activeColor: Colors.orange,
-                    ),
+                    Icon(isNotificationOn ? Icons.notifications_active : Icons.notifications_off, color: isNotificationOn ? Colors.orange : Colors.red),
+                    Switch(value: isNotificationOn, onChanged: (value) => _toggleNotification(), activeColor: Colors.orange),
                   ],
                 ),
               ],
@@ -152,33 +127,18 @@ class _HomePageState extends State<HomePage>
               firstDay: DateTime(2023, 1, 1),
               lastDay: DateTime(2024, 12, 31),
               focusedDay: _focusedDay,
-              selectedDayPredicate: (day) {
-                return isSameDay(_selectedDay, day);
-              },
-              onDaySelected: (selectedDay, focusedDay) {
-                setState(() {
-                  _selectedDay = selectedDay;
-                  _focusedDay = focusedDay; // update `_focusedDay` here as well
-                  print("Selected day: $_selectedDay");
-                });
-              },
+              selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+              onDaySelected: (selectedDay, focusedDay) => setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              }),
               eventLoader: _getEventsForDay,
-              calendarBuilders: CalendarBuilders(
-                markerBuilder: (context, date, events) {
-                  if (events.isNotEmpty) {
-                    return Positioned(
-                      bottom: 1,
-                      child: _buildEventsMarker(),
-                    );
-                  }
-                  return Container();
-                },
-              ),
+              calendarBuilders: CalendarBuilders(markerBuilder: (context, date, items) {
+                return items.isNotEmpty ? Positioned(bottom: 1, child: _buildEventsMarker()) : Container();
+              }),
             ),
           ),
-          Expanded(
-            child: _buildEventList(),
-          ),
+          Expanded(child: _buildEventList()),
         ],
       ),
       bottomNavigationBar: CustomBottomNavigationBar(currentIndex: 0, onTap: (index) {}),
@@ -186,15 +146,15 @@ class _HomePageState extends State<HomePage>
   }
 
   Widget _buildEventList() {
-    final events = _getEventsForDay(_selectedDay!);
-    print("Events for selected day ($_selectedDay): $events");
-    if (events.isEmpty) {
+    final items = _getEventsForDay(_selectedDay!);
+    if (items.isEmpty) {
       return Center(child: Text('No events found'));
     }
     return ListView.builder(
-      itemCount: events.length,
+      itemCount: items.length,
       itemBuilder: (context, index) {
-        var event = events[index];
+        var event = items[index];
+        print("data ${event['title']}"); // In dữ liệu ra để kiểm tra
         return Card(
           margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
           child: ListTile(
@@ -220,10 +180,7 @@ class _HomePageState extends State<HomePage>
 
   Widget _buildEventsMarker() {
     return Container(
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.blue,
-      ),
+      decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.blue),
       width: 7.0,
       height: 7.0,
     );
--- noti_app/lib/main.dart
+++ noti_app/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'dart:io';
+import 'package:noti_app/login/login.dart';
 import 'firebase_options.dart';
 
-void main() async {
+
+
+//void main() => runApp(MyApp());
+
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  //await FirebaseApi().initNotifications();
   runApp(MyApp());
 }
 
@@ -15,99 +18,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase Realtime Demo',
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  final databaseRef = FirebaseDatabase.instance.ref().child('data');
-  List<Map<dynamic, dynamic>> items = [];
-
-  @override
-  void initState() {
-    super.initState();
-    databaseRef.onValue.listen((event) {
-      final data = event.snapshot.value;
-      if (data is Map) {
-        final List<Map<dynamic, dynamic>> loadedItems = [];
-        data.forEach((key, value) {
-          loadedItems.add(Map<dynamic, dynamic>.from(value));
-        });
-        setState(() {
-          items = loadedItems;
-        });
-      } else if (data is List) {
-        final List<Map<dynamic, dynamic>> loadedItems = data.map((item) => Map<dynamic, dynamic>.from(item)).toList();
-        setState(() {
-          items = loadedItems;
-        });
-      }
-    });
-  }
-
-  Future<void> writeDataToCSV() async {
-    try {
-      final directory = await getApplicationDocumentsDirectory();
-      final path = '${directory.path}/assets';
-      final filePath = '$path/data.csv';
-
-      // Kiểm tra và tạo thư mục nếu chưa tồn tại
-      final directoryExists = await Directory(path).exists();
-      if (!directoryExists) {
-        await Directory(path).create(recursive: true);
-      }
-
-      final file = File(filePath);
-
-      // Dữ liệu mẫu để ghi vào file CSV
-      String csvData = "title,description,time,datetime\n";
-
-      for (var item in items) {
-        final title = item['title'] ?? 'No title';
-        final description = item['description'] ?? 'No description';
-        final time = item['time'] ?? 'No time';
-        final datetime = item['datetime'] ?? 'No datetime';
-        csvData += '$title,$description,$time,$datetime\n';
-      }
-
-      // Ghi đè dữ liệu vào file CSV
-      await file.writeAsString(csvData);
-      print('File CSV đã được ghi đè thành công.');
-    } catch (e) {
-      print('Có lỗi xảy ra: $e');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Firebase Realtime Database'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.save),
-            onPressed: writeDataToCSV,
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          final item = items[index];
-          return ListTile(
-            title: Text(item['title'] ?? 'No title'),
-            subtitle: Text(item['description'] ?? 'No description'),
-            trailing: Text(item['datetime'] ?? 'No datetime'),
-          );
-        },
-      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
     );
   }
 }
